c44eef0ae83cf2a295c84cf66edb2e4521ee8984
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-11-27 23:37:53 +0530

    knapsack memoization

--- knapsack.cpp
+++ knapsack.cpp
@@ -3,18 +3,25 @@ using namespace std;
 
 int knapsack(int wt[],int val[],int capacity,int n)
 {
+    //jo value change ho rha usi ko matrix ka size bnate hai memoization me
+	int t[n+1][capacity+1];
+	memset(t,-1,sizeof(t));
 	if(n==0||capacity==0)
 	{
 		return 0;
 	}
+	if(t[n][capacity]!=-1)
+	{
+		return t[n][capacity];
+	}
 
 	if(wt[n-1]<=capacity)
 	{
-		return max(val[n-1]+knapsack(wt,val,capacity-wt[n-1],n-1),knapsack(wt,val,capacity,n-1));
+		return t[n][capacity]= max(val[n-1]+knapsack(wt,val,capacity-wt[n-1],n-1),knapsack(wt,val,capacity,n-1));
 	}
 	else if(wt[n-1]>capacity)
 	{
-		return knapsack(wt,val,capacity,n-1);
+		return t[n][capacity]= knapsack(wt,val,capacity,n-1);
 	}
 }
 int main()
