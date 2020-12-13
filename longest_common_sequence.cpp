21e433f01424ba97dd2709859c8c7b5089023ae8
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2020-12-13 20:11:21 +0530

    longest_common_sequence

---
+++ longest_common_sequence.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int longest_increasing_subsequence(string s1,string s2)
+{
+	
+
+	int n=s1.size();
+	int m=s2.size();
+	int t[n+1][m+1];
+	for(int i=0;i<=n;i++)
+	{
+		t[i][0]=0;
+	}
+	for(int i=0;i<m;i++)
+	{
+		t[0][i]=0;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(int j=1;j<=m;j++)
+		{
+			if(s1[n-i]==s2[m-j])
+			{
+				t[i][j]=1+t[i-1][j-1];
+			}
+			else
+			{
+				t[i][j]=max(t[i][j-1],t[i-1][j]);
+			}
+		}
+	}
+	return t[n][m];
+
+}
+int main()
+{
+	 #ifndef ONLINE_JUDGE
+  freopen("input.txt","r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif
+	string s1,s2;
+	cin>>s1>>s2;
+
+	cout<<longest_increasing_subsequence(s1,s2);
+}
